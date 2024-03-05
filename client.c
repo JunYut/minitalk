273803807e08aeec5e09a5057028559fba4d0801
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2024-03-05 10:57:52 +0800

    Find solution to get bits of a number:
    for(int j = 0; j < 256; ++j)
    {
    	ft_printf("%d: ", j);
    	for(int i = 0; i < 8; ++i)
    		ft_printf("%d", (j >> i) & 1);
    	ft_printf("\n");
    }

--- client.c
+++ client.c
@@ -6,24 +6,31 @@
 /*   By: tjun-yu <tjun-yu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 14:49:36 by tjun-yu           #+#    #+#             */
-/*   Updated: 2024/03/04 15:39:20 by tjun-yu          ###   ########.fr       */
+/*   Updated: 2024/03/05 10:56:36 by tjun-yu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <signal.h>
-#include "../libft/c_libft.h"
+#include "libft/c_libft.h"
 
 void	send(int pid, const char *message)
 {
 	if (message == NULL)
 		return ;
-	while (*message != 0)
+	while (*message != 0);
 }
 
 int	main(void)
 {
-	ft_printf("pid: %d\n", getpid());
-	ft_printf("SIGUSR1: %d\n", SIGUSR1);
-	ft_printf("SIGUSR2: %d\n", SIGUSR2);
+	int num;
+
+	num = 3;
+	for(int j = 0; j < 256; ++j)
+	{
+		ft_printf("%d: ", j);
+		for(int i = 0; i < 8; ++i)
+			ft_printf("%d", (j >> i) & 1);
+		ft_printf("\n");
+	}
 }
