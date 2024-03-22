ac9db06f6e81eec47d824551eeed45b093f713e4
Tan Jun Yu <tjun-yu@u91z05s05.42kl.edu.my>
2024-03-22 14:19:49 +0800

    Need to optimise the transaction of signals, basically redoing the server side

---
+++ libft/c_libft.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   c_libft.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjun-yu <tjun-yu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/29 12:33:04 by we                #+#    #+#             */
+/*   Updated: 2024/03/11 10:16:50 by tjun-yu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef C_LIBFT_H
+# define C_LIBFT_H
+
+# include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
+# include "get_next_line/get_next_line.h"
+
+#endif
